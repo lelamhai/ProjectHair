792826439e872de2f7b412e99600cea3d96ba906
DESKTOP-0H7VB4A\Hai <haill>
2021-05-09 00:56:35 +0700

    contact

--- app/Http/Controllers/AllController/ContactController.php
+++ app/Http/Controllers/AllController/ContactController.php
@@ -4,11 +4,40 @@ namespace App\Http\Controllers\AllController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Mail;
 
 class ContactController extends Controller
 {
 	public function index() {
 		return view('_allView.contact');
 	}
-    //
+
+
+	public function contactSubmit(Request $request)
+	{
+		$data = [
+			'fullName' => $request->fullName,
+			'mail' => $request->mail,
+			'phone' => $request->phone,
+			'description' =>  $request->description
+		];
+
+		Mail::send('_allView.template_contact',
+		[
+			'fullName' => $request->fullName,
+			'mail' => $request->mail,
+			'phone' => $request->phone,
+			'description' => $request->description
+		], function ($message) use($request){
+			// Reset Password
+			// $message->from('leelamhair@gmail.com', 'Admin');
+			// $message->to($request->mail,  $request->fullName);
+
+			$message->from($request->mail,  $request->fullName);
+			$message->to('leelamhair@gmail.com', 'Admin');
+			
+			$message->subject('Khách Hàng Liên Hệ');
+		});
+
+	}
 }
---
+++ resources/views/_allView/template_contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>Liên Hệ</h1>
+<p>Full Name: {{$fullName}}</p>
+<p>mail: {{$mail}}</p>
+<p>phone: {{$phone}}</p>
+<p>description: {{$description}}</p>
+
+</body>
+</html>
