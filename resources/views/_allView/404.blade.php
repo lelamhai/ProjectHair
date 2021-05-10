b41382bf8c80e9df0d32aca6fb49688ec75448ac
DESKTOP-0H7VB4A\Khang <Khang>
2021-05-10 22:30:14 +0700

    page 404 not found

---
+++ resources/views/_allView/404.blade.php
@@ -0,0 +1,14 @@
+@extends('index')
+
+@section('content')
+
+<main>
+    Trang này không tồn tại  404 not found    
+</main>
+
+@endsection
+@section('css')
+@endsection
+
+@section('script')
+@endsection
