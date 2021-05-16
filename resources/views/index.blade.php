3cfecf04ae853081ff6f6e0fc045f45cd4e4369e
Quang Trương <quangklynk@gmail.com>
2021-05-16 23:39:18 +0700

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,117 +74,8 @@
                         </a> -->
 
 
-                        <!-- login start -->
-                        <div id="myModal" class="modal fade" role="dialog">
-                            <div class="modal-dialog">
 
-                                <!-- Modal content-->
-                                <div class="modal-content">
 
-                                    <div class="modal-body">
-                                        <button type="button" class="close off-log" data-dismiss="modal">&times;</button>
-                                        <div class="wrap-form">
-                                            <ul class="nav nav-tabs">
-                                                <li class="active"><a data-toggle="tab" href="#log-in">Đăng Nhập</a></li>
-                                                <li><a data-toggle="tab" href="#sign-up">Đăng Kí</a></li>
-                                            </ul>
-                                            <div class="logo">
-                                                <img src="assets/img/Log/log-30shine-white.9945e644.jpg" alt="" />
-                                            </div>
-                                            <div class="tab-content">
-                                                <div id="log-in" class="tab-pane fade in active">
-                                                    <div class="form-container sign-in-container">
-                                                        <form action="https://www.youtube.com/watch?v=ybFVSDUZhaQ&list=RDEV-91JV4Fws&index=8" id="sign-in-container">
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_letter.png" alt="">
-                                                                </div>
-                                                                <input type="email" placeholder="Email" />
-                                                            </div>
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_lock.png" alt="">
-                                                                </div>
-                                                                <input type="password" placeholder="Mật Khẩu" />
-                                                            </div>
-                                                            <div class="cappcha-input">
-                                                                <div id="divGenerateRandomValues"></div>
-                                                                <input type="text" id='txtInput' />
-                                                            </div>
-                                                            <p class="forgot" id="slidedown">Forgot your password?</p>
-                                                            <button class="signIn-form-button" id="btn-log-in" type="button">Sign In</button>
-                                                        </form>
-                                                    </div>
-                                                    <div class="hide" id="forgot">
-                                                        <form action="" class="form-container">
-                                                            <div class="enter-email">
-                                                                <div class="enter-email-detail">
-                                                                    <h3>Quên mật khẩu</h3>
-                                                                    <div class="account-input">
-                                                                        <div class="icon-form">
-                                                                            <img src="assets/img/Log/icon_letter.png" alt="">
-                                                                        </div>
-                                                                        <input type="email" placeholder="Email" />
-                                                                    </div>
-                                                                    <div class="">
-                                                                        <button class="signIn-form-button">Send</button>
-                                                                        <p id="slideup" style="cursor: pointer"><u>close</u></p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                                <div id="sign-up" class="tab-pane fade">
-                                                    <div class="form-container sign-up-container">
-                                                        <form action="#" id="sign-up-form">
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_user.png" alt="">
-                                                                </div>
-                                                                <input type="text" placeholder="Họ Tên" id="name" />
-                                                            </div>
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_phone.png" alt="">
-                                                                </div>
-                                                                <input type="text" placeholder="Số Điện Thoại" id="phone" />
-                                                            </div>
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_letter.png" alt="">
-                                                                </div>
-                                                                <input type="email" placeholder="Email" id="email" />
-                                                            </div>
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_lock.png" alt="">
-                                                                </div>
-                                                                <input type="password" placeholder="Mật Khẩu" id="pass" />
-                                                            </div>
-                                                            <div class="account-input">
-                                                                <div class="icon-form">
-                                                                    <img src="assets/img/Log/icon_lock.png" alt="">
-                                                                </div>
-                                                                <input type="password" placeholder="Nhập Lại Mật Khẩu" class="last-child" id="repass" />
-                                                            </div>
-                                                            <div class="cappcha-input">
-                                                                <div id="divGenerateRandomValues1"></div>
-                                                                <input type="text" id='txtInput1' />
-                                                            </div>
-                                                            <button class="mt-3 signUp-form-button" id="btn-sign-up" type="button">Sign Up</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            </section>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                            <!-- login end -->
                     </nav>
 
 
@@ -192,7 +83,118 @@
             </div>
         </div>
     </header>
+    <!-- login start -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
 
+            <!-- Modal content-->
+            <div class="modal-content">
+
+                <div class="modal-body">
+                    <button type="button" class="close off-log" data-dismiss="modal">&times;</button>
+                    <div class="wrap-form">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#log-in">Đăng Nhập</a></li>
+                            <li><a data-toggle="tab" href="#sign-up">Đăng Kí</a></li>
+                        </ul>
+                        <div class="logo">
+                            <img src="assets/img/Log/log-30shine-white.9945e644.jpg" alt="" />
+                        </div>
+                        <div class="tab-content">
+                            <div id="log-in" class="tab-pane fade in active">
+                                <div class="form-container sign-in-container">
+                                    <form action="https://www.youtube.com/watch?v=ybFVSDUZhaQ&list=RDEV-91JV4Fws&index=8" id="sign-in-container">
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_letter.png" alt="">
+                                            </div>
+                                            <input type="email" placeholder="Email" />
+                                        </div>
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_lock.png" alt="">
+                                            </div>
+                                            <input type="password" placeholder="Mật Khẩu" />
+                                        </div>
+                                        <div class="cappcha-input">
+                                            <div id="divGenerateRandomValues"></div>
+                                            <input type="text" id='txtInput' />
+                                        </div>
+                                        <p class="forgot" id="slidedown">Forgot your password?</p>
+                                        <button class="signIn-form-button" id="btn-log-in" type="button">Sign In</button>
+                                    </form>
+                                </div>
+                                <div class="hide" id="forgot">
+                                    <form action="" class="form-container">
+                                        <div class="enter-email">
+                                            <div class="enter-email-detail">
+                                                <h3>Quên mật khẩu</h3>
+                                                <div class="account-input">
+                                                    <div class="icon-form">
+                                                        <img src="assets/img/Log/icon_letter.png" alt="">
+                                                    </div>
+                                                    <input type="email" placeholder="Email" />
+                                                </div>
+                                                <div class="">
+                                                    <button class="signIn-form-button">Send</button>
+                                                    <p id="slideup" style="cursor: pointer"><u>close</u></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div id="sign-up" class="tab-pane fade">
+                                <div class="form-container sign-up-container">
+                                    <form action="#" id="sign-up-form">
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_user.png" alt="">
+                                            </div>
+                                            <input type="text" placeholder="Họ Tên" id="name" />
+                                        </div>
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_phone.png" alt="">
+                                            </div>
+                                            <input type="text" placeholder="Số Điện Thoại" id="phone" />
+                                        </div>
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_letter.png" alt="">
+                                            </div>
+                                            <input type="email" placeholder="Email" id="email" />
+                                        </div>
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_lock.png" alt="">
+                                            </div>
+                                            <input type="password" placeholder="Mật Khẩu" id="pass" />
+                                        </div>
+                                        <div class="account-input">
+                                            <div class="icon-form">
+                                                <img src="assets/img/Log/icon_lock.png" alt="">
+                                            </div>
+                                            <input type="password" placeholder="Nhập Lại Mật Khẩu" class="last-child" id="repass" />
+                                        </div>
+                                        <div class="cappcha-input">
+                                            <div id="divGenerateRandomValues1"></div>
+                                            <input type="text" id='txtInput1' />
+                                        </div>
+                                        <button class="mt-3 signUp-form-button" id="btn-sign-up" type="button">Sign Up</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- login end -->
     @yield('content')
 
     <footer class="footer" id="footer">
