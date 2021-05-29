f3fe11329491939c6f542be8c55a25227a2c2d4c
DESKTOP-0H7VB4A\Hai <haill>
2021-05-30 00:48:16 +0700

    menu admin

--- resources/views/_adminView/index.blade.php
+++ resources/views/_adminView/index.blade.php
@@ -35,13 +35,24 @@
                         <span></span>
                     </span>
                     <div class="left__content">
-                        <div class="left__logo">Dashboard</div>
                         <div class="left__profile">
                             <div class="left__image"><img src="/assets/admin/assets/avatar.jpg" alt=""></div>
-                            <p class="left__name">Hatsune Miku</p>
+                            <p class="left__name">Tên User</p>
                         </div>
                         <ul class="left__menu">
+                            <li class="left__menuItem">
+                                <div class="left__title">Bảng điều khiển
+                            </li>
 
+                            <li class="left__menuItem">
+                                <div class="left__title">
+									Sản Phẩm<i class="fas fa-plus"></i>
+                                </div>
+                                <div class="left__text">
+                                    <a class="left__link" href="insert_product.html">Chèn Sản Phẩm</a>
+                                    <a class="left__link" href="view_product.html">Xem Sản Phẩm</a>
+                                </div>
+                            </li>
 
                             <li class="left__menuItem">
                                 <div class="left__title">
@@ -54,172 +65,26 @@
                                 </div>
                             </li>
 
-
-
-
-
-							<li class="left__menuItem">
-                                <div class="left__title">
-									Sản Phẩm<i class="fas fa-plus"></i>
-                                </div>
-                                <div class="left__text">
-                                    <a class="left__link" href="insert_product.html">Chèn Sản Phẩm</a>
-                                    <a class="left__link" href="view_product.html">Xem Sản Phẩm</a>
-                                </div>
-                            </li>
                             <li class="left__menuItem">
-                                <div class="left__title">Danh Mục SP
-                                    <i class="fas fa-plus"></i>
-                                </div>
-                                <div class="left__text">
-                                    <a class="left__link" href="insert_p_category.html">Chèn Danh Mục</a>
-                                    <a class="left__link" href="view_p_category.html">Xem Danh Mục</a>
-                                </div>
+                                <div class="left__title">Mẫu tóc
                             </li>
+
                             <li class="left__menuItem">
-                                <div class="left__title">Thể Loại
-                                    <i class="fas fa-plus"></i>
-                                </div>
-                                <div class="left__text">
-                                    <a class="left__link" href="insert_category.html">Chèn Thể Loại</a>
-                                    <a class="left__link" href="view_category.html">Xem Thể Loại</a>
-                                </div>
+                                <div class="left__title">Quản lý đơn hàng
                             </li>
+
+                            <li class="left__menuItem">
+                                <div class="left__title">Đặt vé
+                            </li>
+							
                             <li class="left__menuItem">
                                 <div class="left__title">Đăng xuất
-
                             </li>
                         </ul>
                     </div>
                 </div>
-                <section class="right">
-                    <div class="right__content">
-                        <div class="right__head d-flex">
-                            <div class="right__head-post d-flex">
-                                <h5 class="right__head-post-title">Posts</h5>
-                                <a href="./NewPost.html" class="right__head-post-add">Add New</a>
-                            </div>
-                            <div class="right__head-search">
-                                {{-- <input type="text" class="right__head-search-input">
-                                <button type="button" class="right__head-search-btn">Search Posts</button> --}}
-                            </div>
-                        </div>
-                        <div class="right__head-bar d-flex">
-                            <div class="right__head-bar-select">
-                                {{-- <input type="checkbox" id="all" class="right__head-bar-checkbox">
-                                <label for="all" class="right__head-bar-label">All</label>
-                                <button class="right__head-bar-delete">Delete</button> --}}
-                            </div>
-                            <span class="right__head-bar-count">2 items</span>
-                        </div>
-
-                        <div class="right__table">
-                            <div class="right__tableWrapper">
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th style="text-align: left;">Title</th>
-                                            <th>Categories</th>
-                                            <th>Date</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-
-
-                                        <tr class="right__table-item">
-                                            <td data-label="Email" style="text-align: left;">
-                                                {{-- <input type="checkbox" name="" id=""> --}}
-                                                bài viết số 123
-                                                <div class="right__table-item-change">
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--edit">Edit</button>
-                                                    |
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--delete">Delete</button>
-                                                </div>
-                                            </td>
-                                            <td data-label="Số Hoá Đơn">Uncategorized</td>
-                                            <td data-label="ID Sản Phẩm">2/5/2021</td>
-
-                                        </tr>
-
-
-
-										
-                                        <tr class="right__table-item">
-                                            <td data-label="Email" style="text-align: left;">
-												{{-- <input type="checkbox" name="" id=""> --}}
-													 bài viết số 123
-                                                <div class="right__table-item-change">
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--edit">Edit</button>
-                                                    |
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--delete">Delete</button>
-                                                </div>
-                                            </td>
-                                            <td data-label="Số Hoá Đơn">Uncategorized</td>
-                                            <td data-label="ID Sản Phẩm">4/5/2021</td>
-
-                                        </tr>
-                                        <tr class="right__table-item">
-                                            <td data-label="Email" style="text-align: left;">
-												{{-- <input type="checkbox" name="" id=""> --}}
-													 bài viết số 123
-                                                <div class="right__table-item-change">
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--edit">Edit</button>
-                                                    |
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--delete">Delete</button>
-                                                </div>
-                                            </td>
-                                            <td data-label="Số Hoá Đơn">Categorized</td>
-                                            <td data-label="ID Sản Phẩm">3/5/2021</td>
-
-                                        </tr>
-                                        <tr class="right__table-item">
-                                            <td data-label="Email" style="text-align: left;">
-												{{-- <input type="checkbox"name="" id="">  --}}
-												bài viết số 123
-                                                <div class="right__table-item-change">
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--edit">Edit</button>
-                                                    |
-                                                    <button
-                                                        class="right__table-item-btn right__table-item-btn--delete">Delete</button>
-                                                </div>
-                                            </td>
-                                            <td data-label="Số Hoá Đơn">9659544</td>
-                                            <td data-label="ID Sản Phẩm">8/5/2021</td>
-
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <div class="right_pagination d-flex">
-                            <span class="right_pagination-item">
-                                1
-                            </span>
-                            <span class="right_pagination-item">
-                                2
-                            </span>
-                            <span class="right_pagination-item">
-                                3
-                            </span>
-
-                            <span class="right_pagination-item">
-                                ...
-                            </span>
-
-                            <span class="right_pagination-item">
-                                <i class="fas fa-arrow-right"></i>
-                            </span>
-                        </div>
-                    </div>
-                </section>
+                @yield('content')
+	            @csrf 
             </div>
         </div>
     </div>
